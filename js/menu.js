6fbb909189434371388368551e8bc892cf1c1196
CrowdHailer <peterhsaxton@gmail.com>
2013-11-08 16:42:58 +0000

    Switch/Case handling of button events
    Switch Case handling of click evens on <a> in list nest. Random content
    behaviour to main paragraph

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,26 @@ var myApp = {
     init: function () {
         $('ul li a').click(function () {
             $(this).toggleClass('active');
+			var recordString = "";
+			switch(this.id) {
+				case "suburban-toggle":
+					recordString = "Just Suburbaned it up";
+					break;
+				case "zones-toggle":
+					recordString = "this is the second button";
+					break;
+				case "wheelchair-toggle":
+					recordString = "All about the wheelchairs";
+					break;
+				case "cycle-toggle":
+					recordString = "End of the line";
+					break;
+			}
+			myApp.output.innerHTML += recordString + "<br>";
         });
+		this.output = document.getElementById("output");
     }
+	
 };
 $(document).ready(function () {
     myApp.init();
