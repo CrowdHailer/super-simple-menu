5e4b37023a6c9e5a4828d26f142f35b77de89cc5
CrowdHailer <peterhsaxton@gmail.com>
2013-11-11 09:51:00 +0000

    Set Content to maximum Height
    Set content to not exceed page height, needs to be expanded to consider
    footer and orientation change

--- js/menu.js
+++ js/menu.js
@@ -14,13 +14,22 @@ var myApp = {
 					recordString = "All about the wheelchairs";
 					break;
 				case "cycle-toggle":
-					recordString = "End of the line";
+					recordString = "";
+					myApp.output.innerHTML = "";
+					alert("Cleared click record");
 					break;
 			}
 			myApp.output.innerHTML += recordString + "<br>";
         });
 		this.output = document.getElementById("output");
-    }
+		this.setToPageHeight(this.output);
+    },
+	setToPageHeight: function (jqItem) {
+		var pageHeight = $(window).height();
+		var topOffset = $(jqItem).offset().top;
+		var itemHeight = pageHeight - topOffset;
+		$(jqItem).css({"max-height": itemHeight + 'px' });
+	}
 	
 };
 $(document).ready(function () {
